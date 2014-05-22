f5b81fb2270f613c55c5631da5011514a9a9ad35
Sam Whale <swhale6988@sohamcollege.org.uk>
2014-05-22 16:39:21 +0100

    Update diceRoller.py
    Debugged, changed some data structures and completed most tasks

--- diceRoller.py
+++ diceRoller.py
@@ -1,34 +1,45 @@
 #Coin flip program
-#Describe the purpose of this program here.
+#The Purpose of this program is to generate a random number from 1 to 6 and display it
+#As a dice face. I have then chnaged the program to continue until
+#it has rolled a 6
+#<(^_^<) <(^_^)> (>^_^)>
 
-inport random;time
-
-s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
-s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
-s3 = "- - - - -\n| O     |\n|   O   |\n|     O |\n- - - - -\n"
-s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n"
-s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
-s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
+import random,time
+result = ""
+result2 = " "  
+dice = ["- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n",
+"- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n",
+"- - - - -\n| O     |\n|   O   |\n|     O |\n- - - - -\n",  #This part stores the different dice graphics into 6 different variables in a list.
+"- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n",
+"- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n",
+"- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"]
 
 def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
+    print("rolling.....")
+    global result  #This part tells the user the dice is rolling and stores the result in a global variable
+    result = random.randint(1,6)
 
 
-def show_dice(roll)
-    if roll = 1:
-        print(S1)
-    else roll = 2
-        print(s2)
-    else roll = 3
-        print(s3)
-    else roll = 4
-        print(s4)
-    else roll = 5
-        print(s5)
-    else roll = 6
-        print(s6)
 
-roll
-time.sleep(1)
-show_dice(roll)
+
+def show_dice():
+    print(dice[result-1])  #This prints the dice depending on the roll
+
+
+while result != 6:
+    userinput = input("Press enter to continue")  # This forces the user to press enter to continue the program
+    roll() #Runs teh roll function
+    time.sleep(1) #Pauses the program
+    show_dice() #runs the roll dice function
+    
+
+"""
+inport instead of import
+; instead of ,
+Print instead of print
+randing instead of randint
+else instead of elif
+multiple lack of colons (>^_^)>
+"""
+
+
